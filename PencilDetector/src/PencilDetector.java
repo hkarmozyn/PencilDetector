c1f6ad2241e5b5e2b2ec98778cc4ed238d7ab818
John Karmozyn <hughkarmozyn@icloud.com>
2024-04-16 12:23:20 -0600

    added methods to take in and resize image

--- PencilDetector/src/PencilDetector.java
+++ PencilDetector/src/PencilDetector.java
@@ -1,2 +1,42 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class PencilDetector {
+
+    private static BufferedImage readImage(String filePath) throws IOException {
+        File imageFile = new File(filePath);
+        return ImageIO.read(imageFile);
+    }
+
+    private static BufferedImage resizeImage(BufferedImage image, int targetWidth, int targetHeight) {
+        Image tmp = image.getScaledInstance(targetWidth, targetHeight, Image.SCALE_SMOOTH);
+        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = resizedImage.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+        return resizedImage;
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            BufferedImage image = readImage("input.jpg");
+
+            int width = image.getWidth();
+            int height = image.getHeight();
+            System.out.println("Image width: " + width + " pixels");
+            System.out.println("Image height: " + height + " pixels");
+
+            int targetWidth = 300;
+            int targetHeight = 200;
+            BufferedImage resizedImage = resizeImage(image, targetWidth, targetHeight);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
