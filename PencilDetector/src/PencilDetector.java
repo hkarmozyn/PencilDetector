d74aa290ea8ed8031aec4964f6904856ff855afa
John Karmozyn <hughkarmozyn@icloud.com>
2024-04-19 14:23:09 -0600

    added images and methods so that the image is converted in the gray scale!

--- PencilDetector/src/PencilDetector.java
+++ PencilDetector/src/PencilDetector.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class PencilDetector {
 
@@ -12,12 +13,28 @@ public class PencilDetector {
     }
 
     private static BufferedImage resizeImage(BufferedImage image, int targetWidth, int targetHeight) {
+        int originalWidth = image.getWidth();
+        int originalHeight = image.getHeight();
+        double aspectRatio = (double) originalWidth / originalHeight;
+        if (originalWidth > originalHeight) {
+            targetHeight = (int) (targetWidth / aspectRatio);
+        } else {
+            targetWidth = (int) (targetHeight * aspectRatio);
+        }
         Image tmp = image.getScaledInstance(targetWidth, targetHeight, Image.SCALE_SMOOTH);
         BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2d = resizedImage.createGraphics();
         g2d.drawImage(tmp, 0, 0, null);
         g2d.dispose();
         return resizedImage;
+    }
+
+    public static BufferedImage processImage(BufferedImage image) {
+
+        BufferedImage grayImage = convertToGrayscale(image);
+        BufferedImage gaussianImage = applyGaussianBlur(grayImage);
+
+        return gaussianImage;
     }
 
     private static BufferedImage convertToGrayscale(BufferedImage image) {
@@ -40,17 +57,44 @@ public class PencilDetector {
 
         return grayImage;
     }
-    public static BufferedImage processImage(BufferedImage image) {
 
-        BufferedImage grayImage = convertToGrayscale(image);
+    private static BufferedImage applyGaussianBlur(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
 
-        return grayImage;
+        BufferedImage blurredImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+
+        double[][] kernel = {
+                {1.0 / 16, 1.0 / 8, 1.0 / 16},
+                {1.0 / 8, 1.0 / 4, 1.0 / 8},
+                {1.0 / 16, 1.0 / 8, 1.0 / 16}
+        };
+
+        for (int y = 1; y < height - 1; y++) {
+            for (int x = 1; x < width - 1; x++) {
+                double sum = 0.0;
+                for (int i = -1; i <= 1; i++) {
+                    for (int j = -1; j <= 1; j++) {
+                        int pixel = image.getRGB(x + i, y + j) & 0xFF;
+                        sum += kernel[i + 1][j + 1] * pixel;
+                    }
+                }
+                int blurredPixel = (int) Math.round(sum);
+                blurredImage.setRGB(x, y, blurredPixel << 16 | blurredPixel << 8 | blurredPixel);
+            }
+        }
+
+        return blurredImage;
     }
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Input File Name");
+
+         String input = s.nextLine();
 
         try {
-            BufferedImage image = readImage("input.jpg");
+            BufferedImage image = readImage(input);
 
             int width = image.getWidth();
             int height = image.getHeight();
@@ -59,7 +103,16 @@ public class PencilDetector {
 
             int targetWidth = 300;
             int targetHeight = 200;
+
             BufferedImage resizedImage = resizeImage(image, targetWidth, targetHeight);
+            BufferedImage processedImage = processImage(resizedImage);
+
+            File outputImageFile = new File("processed_image.jpg");
+
+            ImageIO.write(processedImage, "jpg", outputImageFile);
+
+            System.out.println("Processed image saved successfully!");
+
 
         } catch (IOException e) {
             e.printStackTrace();
